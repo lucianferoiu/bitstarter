a33e16dd3428a6d04d197d9ee7bcfc21d0d0dc0f
Lucian Feroiu <lucianferoiu@gmail.com>
2013-07-09 15:17:35 +0000

    Add index.html and made web.js to use it's content

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+function readIndex() {
+    var fname='index.html';
+    var buf = fs.readFileSync(fname);
+    console.assert(Buffer.isBuffer(buf),'The readFileSync did not return a Buffer');
+    console.assert(buf.length>0,'index.html is empty');
+    var c = buf.toString();
+    return c;
+}
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var c = readIndex();
+    response.send(c);
 });
 
 var port = process.env.PORT || 5000;
